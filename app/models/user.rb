9336c919fb70d51591e528037edf2a8a030c68a6
vmank <54033614+vmank@users.noreply.github.com>
2021-01-26 08:22:13 +0200

    basic user role setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,17 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  # Permissions for each role are defined in the PostsPolicy
+  enum role: [:normal, :subscriber, :editor, :moderator, :admin]
+
+
+  after_initialize :set_default_role
+
+
+  private
+
+  def set_default_role
+    self.role ||= :normal
+  end
 end
