41190d58b8cbb31a4b35306cfca432f057baa91c
vmank <54033614+vmank@users.noreply.github.com>
2021-01-26 09:44:36 +0200

    post statuses enum and default status

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+  after_initialize :set_default_status
+
+  # Post statuses
+  #   deleted
+  #   pending = pending review
+  #   published = visible for everyone
+  #   restricted = visible for users with subscriber role and above
+  enum status: [:deleted, :pending, :published, :restricted]
+
+
+  private
+
+  def set_default_status
+    self.status ||= :pending
+  end
 end
